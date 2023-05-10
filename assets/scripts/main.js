28b3437882328b9e9a9021fe6533cc0818800276
Sula Michele <sulamichele95@gmail.com>
2023-05-10 08:46:34 +0200

    Removed grid creation from for cicle

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,24 +1,26 @@
 const playButton = document.getElementById("play-button");
 const gridContainer = document.getElementById("main-section");
-const difficultySelect = document.getElementById("select");
 
 
 playButton.addEventListener("click", 
     function () {
         // Check for grid already existing
-        const gridToEliminate = gridContainer.querySelector('.grid-container');
+        const gridToEliminate = gridContainer.querySelector(".grid-container");
         // If grid exists remove it
         if (gridToEliminate) {
             gridToEliminate.remove();
         }
 
+        // Creates new div with grid-container class
+        const newGrid = createElement("div", "grid-container");
+        gridContainer.append(newGrid);
+        
         // Gets value from the select
         let userChoice = document.getElementById("select").value;
-        // Checks the value and changes the grid
+        // Checks the value and changes the grid difficulty:
+        // EASY
         if (userChoice === "facile") {
-            const newGrid = createElement("div", "grid-container");
-            gridContainer.append(newGrid);
-
+        
             for (let i = 1; i <= 100; i++) {
             const newSquare = createElement("div", "square-10");
             const newText = createElement("h2", "text-white");
@@ -26,10 +28,8 @@ playButton.addEventListener("click",
             newGrid.append(newSquare);
             newSquare.append(newText);
             } 
-
+        // MEDIUM
         } else if (userChoice === "medio") {
-            const newGrid = createElement("div", "grid-container");
-            gridContainer.append(newGrid);
 
             for (let i = 1; i <= 81; i++) {
             const newSquare = createElement("div", "square-9");
@@ -38,10 +38,8 @@ playButton.addEventListener("click",
             newGrid.append(newSquare);
             newSquare.append(newText);
             } 
+        // HARD
         } else {
-
-            const newGrid = createElement("div", "grid-container");
-            gridContainer.append(newGrid);
 
             for (let i = 1; i <= 49; i++) {
             const newSquare = createElement("div", "square-7");
